bf4de3bbbb78dfdde2533523c65564a41204c317
rahul <ra9853044@gmail.com>
2023-12-16 22:31:28 +0530

    removed initialisation of scoreDataBase method from ScoreDataBase class

--- app/src/main/java/com/example/tictactoe/data/ScoreDataBase.kt
+++ app/src/main/java/com/example/tictactoe/data/ScoreDataBase.kt
@@ -8,16 +8,4 @@ import androidx.room.RoomDatabase
 @Database(entities = [CurrScore::class],version = 6, exportSchema = false)
 abstract class ScoreDataBase: RoomDatabase() {
     abstract fun currScoreDao(): CurrScoreDao
-    companion object{
-        @Volatile
-        private var INSTANCE:ScoreDataBase?=null
-        fun getDatabase(context: Context): ScoreDataBase {
-            return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(context, ScoreDataBase::class.java, "score_database")
-                    .fallbackToDestructiveMigration()
-                    .build()
-                    .also { INSTANCE = it }
-            }
-        }
-    }
 }
