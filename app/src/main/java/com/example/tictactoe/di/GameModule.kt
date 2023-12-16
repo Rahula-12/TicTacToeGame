7fbe178980174ca644a46647db98f602c1c45363
rahul <ra9853044@gmail.com>
2023-12-16 22:30:14 +0530

    implemented gameModule module class

---
+++ app/src/main/java/com/example/tictactoe/di/GameModule.kt
@@ -0,0 +1,31 @@
+package com.example.tictactoe.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.tictactoe.data.CurrScoreDao
+import com.example.tictactoe.data.ScoreDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class GameModule {
+
+    @Singleton
+    @Provides
+    fun providesScoreDb(@ApplicationContext context: Context):ScoreDataBase{
+        return Room.databaseBuilder(context, ScoreDataBase::class.java, "score_database")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun providesDao(scoreDataBase: ScoreDataBase):CurrScoreDao{
+        return scoreDataBase.currScoreDao()
+    }
+
+}
